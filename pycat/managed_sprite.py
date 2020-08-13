eeb43bce54ff7bfd5774707387abfc818904a359
dw <white.dh@gmail.com>
2020-08-13 13:28:05 +0800

    ManagedWindow and ManagedSprite + tags remove the need for sprite lists

---
+++ pycat/managed_sprite.py
@@ -0,0 +1,254 @@
+from typing import Tuple, Optional
+from pyglet.sprite import Sprite as PygletSprite
+
+from pycat.geometry.point import Point
+from pycat.image import get_animation_from_file, get_texture_from_file, get_checker_texture
+from pycat.math import get_direction_from_degrees
+from pycat.debug import print_warning
+
+from pycat.collision import is_aabb_collision
+
+from random import randint
+
+class ManagedSprite:
+
+    def __init__(
+        self, 
+        window,
+        tags: [str] = []        
+    ):
+        self.__window = window
+        self.__tags = tags
+
+        img = get_checker_texture()
+        self._sprite: PygletSprite = PygletSprite(img)
+
+        self._layer = 0
+
+
+
+    ################################################################################
+    # Override these to set the sprite's behavior
+    ################################################################################
+
+    def on_create(self):
+        """Called (once) when added to a window.
+        """
+        pass
+
+    def on_update(self, dt):
+        """Called 60 times a second when added to a window. Stops being called when removed from window.
+        """
+        pass
+
+    ################################################################################
+    # Sprite Position
+    ################################################################################
+
+    @property
+    def x(self) -> float:
+        return self._sprite.x
+
+    @x.setter
+    def x(self, x: float):
+        self._sprite.x = x
+
+    @property
+    def y(self) -> float:
+        return self._sprite.y
+
+    @y.setter
+    def y(self, y: float):
+        self._sprite.y = y
+
+    @property
+    def position(self) -> Point:
+        return Point(self._sprite.x, self._sprite.y)        
+
+    @position.setter
+    def position(self, p: Point):
+        self._sprite.x = p.x
+        self._sprite.y = p.y
+
+    def limit_position_to_area(self, min_x, max_x, min_y, max_y):
+        if self.x < min_x:
+            self.x = min_x
+        elif self.x > max_x:
+            self.x = max_x
+        if self.y < min_y:
+            self.y = min_y
+        elif self.y > max_y:
+            self.y = max_y
+
+    def goto(self, other_sprite: 'Sprite'):
+        self.position = other_sprite.position
+
+    def goto_random_position(self):
+        self.x = randint(0, self.__window.width-self.width)
+        self.y = randint(0, self.__window.height-self.height)
+
+    ################################################################################
+    # Sprite Rotation
+    ################################################################################
+
+    @property
+    def rotation(self) -> float:
+        # rotation is clock-wise positive in pyglet
+        return -self._sprite.rotation
+
+    @rotation.setter
+    def rotation(self, degrees: float):
+        # rotation is clock-wise positive in pyglet
+        self._sprite.rotation = -degrees
+
+    def rotate(self, degrees: float):
+        """rotates the sprite by degrees
+
+        Args:
+            degrees (float): positive direction is counter-clockwise
+        """
+        self._sprite.rotation -= degrees
+
+    ################################################################################
+    # Sprite Motion
+    ################################################################################
+
+    def translate(self, x: float, y: float):
+        """Translates the sprite by (x,y)
+
+        Args:
+            x (float): the amount to translate along the x-direction
+            y (float): the amount to translate along the y-direction
+        """
+        self._sprite.x += x
+        self._sprite.y += y
+
+    def move_forward(self, step_size: float):
+        """move the sprite forward by step_size
+
+        Args:
+            step_size (float): measured in pixels
+        """
+        v = get_direction_from_degrees(self.rotation)
+        self.translate(v.x * step_size, v.y * step_size)
+
+    ################################################################################
+    # Sprite Appearance
+    ################################################################################
+
+    @property
+    def scale(self) -> float:
+        return self._sprite.scale
+
+    @scale.setter
+    def scale(self, scale: float):
+        self._sprite.scale = scale
+
+    @property
+    def scale_x(self) -> float:
+        return self._sprite.scale_x
+
+    @scale_x.setter
+    def scale_x(self, scale_x: float):
+        self._sprite.scale_x = scale_x
+
+    @property
+    def scale_y(self) -> float:
+        return self._sprite.scale_y
+
+    @scale_y.setter
+    def scale_y(self, scale_y: float):
+        self._sprite.scale_y = scale_y
+
+    @property
+    def is_visible(self) -> bool:
+        return self._sprite.visible
+
+    @is_visible.setter
+    def is_visible(self, is_visible: bool):
+        self._sprite.visible = is_visible
+
+    @property
+    def layer(self) -> int:
+        return self._layer
+
+    @layer.setter
+    def layer(self, layer: int):
+        self._layer = layer
+
+    @property
+    def color(self) -> Tuple:
+        return self._sprite.color
+
+    @color.setter
+    def color(self, color: Tuple):
+        self._sprite.color = color
+
+    @property
+    def opacity(self) -> int:
+        """opacity measures transparency
+
+        Returns:
+            int: the transparency value in range [0,255]
+            0 being fully transparent and 255 non-transparent
+        """
+        return self._sprite.opacity
+
+    @opacity.setter
+    def opacity(self, opacity: int):
+        """sets the transparency
+
+        Args:
+            opacity (int): the transparency value in range [0,255]
+            0 being fully transparent and 255 non-transparent
+        """
+        self._sprite.opacity = opacity
+
+    @property
+    def width(self) -> float:
+        return self._sprite.width
+
+    @property
+    def height(self) -> float:
+        return self._sprite.height
+
+    def set_image(self, img_file_path: str):
+        if img_file_path.endswith(".gif"):
+            self._sprite.image = get_animation_from_file(img_file_path)
+        else:
+            self._sprite.image = get_texture_from_file(img_file_path)
+
+    def set_animation_dt(self, dt: float):
+        if self._sprite._animation:
+            self._sprite._animate(dt)
+        else:
+            print_warning("this sprite has no animation")
+
+    ################################################################################
+    # Scratch languge
+    ################################################################################
+
+    def change_x(self, step_size: int):
+        self.x += step_size
+
+    ################################################################################
+    # Framework methods
+    ################################################################################
+
+    def draw(self):
+        # draws on current window set by pyglet backend
+        self._sprite.draw()
+
+    def get_tags(self):
+        return self.__tags
+
+    def delete(self):
+        self.__window.delete_sprite(self)
+
+    def touching_any_sprite_with_tag(self, tag):
+        for s in self.__window.get_sprites_with_tag(tag):
+            if is_aabb_collision(self, s):
+                return True
+        return False
+
+    
---
+++ pycat/managed_window.py
@@ -0,0 +1,175 @@
+from pyglet.window import Window as PygletWindow
+from pyglet import app
+from pycat.geometry.point import Point
+from pycat.managed_sprite import ManagedSprite
+from pycat.label import Label
+from pycat.scheduler import Scheduler
+
+from pyglet.clock import schedule_interval as pyglet_schedule_interval
+from pyglet.clock import schedule_once as pyglet_schedule_once
+from pyglet.clock import unschedule as pyglet_unschedule
+
+class ManagedWindow():
+
+    def __init__(
+        self, 
+        width: int = 1280, 
+        height:int = 640, 
+        background_image='',
+        enforce_window_limits=True
+    ):
+        self.__window: PygletWindow = PygletWindow(width,height)
+
+        self.__active_keys: Set[int] = set()
+        self.__window.on_key_press = self.__on_key_press
+        self.__window.on_key_release = self.__on_key_release
+        self.__user_key_press = lambda key,mod: None
+        self.__user_key_release = lambda key,mod: None
+        
+        self.__window.on_draw = self.__auto_draw
+        
+        self.__enforce_window_limits = enforce_window_limits
+        self.__pre_draw_function = lambda: None
+        self.__post_draw_function = lambda: None
+
+        # First element of __sprite_lists reserved for Sprites directly added to the window.
+        # Remainder of list contains SpriteLists that were added by user.
+        self.__sprites: [Sprite] = []
+        self.__tagmap = {}
+
+        self.__labels: [Label] = []
+
+        self.__background_sprite = None
+        if background_image:
+            self.__background_sprite = ManagedSprite(self)
+            self.__background_sprite.set_image(background_image)
+            self.__background_sprite.position = self.get_center()
+
+
+    # Sprite / Label management
+
+    def add_label(self, label: Label):
+        self.__labels.append(label)
+
+    def create_sprite(self, sprite_cls):
+        sprite = sprite_cls(window=self)
+        self.__register_sprite(sprite)
+        return sprite
+
+    def create_sprite_with_tag(self, sprite_cls, tag):
+        sprite = sprite_cls(window=self, tags=[tag])
+        self.__register_sprite(sprite)
+        return sprite        
+
+    def create_sprite_with_tags(self, sprite_cls, tags):
+        sprite = sprite_cls(window=self, tags=tags)
+        self.__register_sprite(sprite)
+        return sprite    
+
+    def delete_sprite(self, sprite):
+        self.__deregister_sprite(sprite)
+        
+
+    def __register_sprite(self, sprite):
+        self.__sprites.append(sprite)
+        for tag in sprite.get_tags():
+            if tag not in self.__tagmap:
+                self.__tagmap[tag] = []
+            self.__tagmap[tag].append(sprite)
+
+        sprite.on_create()
+        pyglet_schedule_interval(sprite.on_update, 1/60)            
+
+    def __deregister_sprite(self, sprite):
+        pyglet_unschedule(sprite.on_update)
+        
+        self.__sprites.remove(sprite)
+        for tag,sprites in self.__tagmap.items():
+            if sprite in sprites:
+                self.__tagmap[tag] = [s for s in self.__tagmap[tag] if s is not sprite]
+
+    def get_sprites_with_tag(self, tag):
+        return self.__tagmap.get(tag, [])
+
+
+    # Drawing
+
+    def clear(self):
+        self.__window.clear()
+
+    def set_pre_draw(self, pre_draw_function):
+        self.__pre_draw_function = pre_draw_function
+
+    def set_post_draw(self, post_draw_function):
+        self.__post_draw_function = post_draw_function
+
+    def __auto_draw(self):
+
+        self.clear()
+
+        self.__pre_draw_function()
+
+        if self.__background_sprite:
+            self.__background_sprite.draw()
+
+        if self.__enforce_window_limits:
+            for s in self.__sprites:
+                s.limit_position_to_area(0, self.width, 0, self.height)
+
+        objects_to_draw = self.__sprites + self.__labels
+        objects_to_draw.sort(key=lambda o: o.layer)
+
+        for o in objects_to_draw:
+            o.draw()       
+
+        self.__post_draw_function() 
+
+
+    # Key input
+
+    def is_active_key(self, keycode: int) -> bool:
+        return keycode in self.__active_keys
+
+    def set_on_key_press(self, key_press_function):
+        self.__user_key_press = key_press_function
+
+    def set_on_key_release(self, key_release_function):
+        self.__user_key_release = key_release_function
+
+    def __on_key_press(self, key, mod):
+        self.__user_key_press(key,mod)
+        self.__active_keys.add(key)
+
+    def __on_key_release(self, key, mod):
+        self.__user_key_release(key,mod)
+        self.__active_keys.remove(key)
+
+    # Runtime
+
+    def run(self):
+        app.run()
+
+    
+    # Helpers
+
+    def get_center(self) -> Point:
+        return Point(self.width/2, self.height / 2)
+
+
+    # Properties
+
+    @property
+    def width(self) -> int:
+        size: Tuple = self.__window.get_size()
+        return size[0]
+
+    @property
+    def height(self) -> int:
+        size: Tuple = self.__window.get_size()
+        return size[1]    
+
+    
+
+    
+
+
